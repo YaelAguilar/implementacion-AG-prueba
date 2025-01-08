38d3555057afcb3ecc888d038023769af3f3d434
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-01-07 22:05:07 -0600

    crear poblacion

--- gen.py
+++ gen.py
@@ -11,12 +11,16 @@ class DNA():
     
     def create_individual(self, min  = 0, max = 9):
         individual = [np.random.randint(min, max) for i in range(len(self.target))]
-        print(individual)
+        return individual
+    
+    def create_population(self):
+        population = [self.create_individual() for i in range(self.n_individuals)]
+        print(population)
 
 def main():
     target = [1, 0, 0, 1 ,0 ,1 ,1]
     model = DNA(target = target, mutation_rate = 0.02, n_individuals = 15, n_selection= 5, n_generations = 50, verbose= False)
-    model.create_individual()
+    model.create_population()
 
 if __name__ == "__main__":
     main()
